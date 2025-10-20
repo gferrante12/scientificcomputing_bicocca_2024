7a37a5b3356bee60decff185460ea6891b3c4f93
gferrante12 <g.ferrante12@campus.unimib.it>
2025-10-20 10:06:48 +0200

    Delete working directory

--- working/L08/myexamplepkg/converter.py
+++
@@ -1,7 +0,0 @@
-def joules_to_calories(joules: float) -> float:
-    """Convert joules to calories."""
-    return joules / 4.184
-
-def calories_to_joules(cal: float) -> float:
-    """Convert calories to joules."""
-    return cal * 4.184
